a1b247e2ad8461ef535d737ee0e864e8806b9510
Mahad-git-ux <mahad@gmail.com>
2026-06-29 13:59:06 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+<scripyt>
+<h1>hello iam Mahdii From Sardheri Bazar Charsadda Pakistan<h1/>    
+   <p>I am a full stack passionate Web Developer and also a student of BS and Dental tecnition 
+    these both dental and BS Educatioin in Currently in Progress.
+    
+   </p>
+    <scripyt>
